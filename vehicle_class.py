11a16a4ddfd9f51c423488f26af0d497c69c3417
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-08 10:56:26 +0000

    vehicle class created

--- vehicle_class.py
+++ vehicle_class.py
@@ -7,3 +7,18 @@
 #methods :
 # accelerate
 # break
+
+
+class Vehicle():
+    pass
+
+    def __init__(self,n_passengers,cargo_size):
+        self.n_passengers = n_passengers
+        self.cargo_size = cargo_size
+
+    def accelerate (self):
+        return 'vroom'
+
+
+    def breaking(self):
+        return 'skrrrrr'
